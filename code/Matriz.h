2c05f6425d47a6aa06ef045d8f85b52a8d1c736c
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-06-19 16:56:39 -0500

    Matriz

--- code/Matriz.h
+++ code/Matriz.h
@@ -19,9 +19,41 @@ public:
             matriz[i] = new T[columnas];
     }
 
+    T get(int i, int j){
+        return matriz[i][j];
+    }
+    T get_filas(){
+        return filas;
+    }
+    T get_columnas(){
+        return columnas;
+    }
+
     void set(int pos_fila, int pos_columna, T valor){
         matriz[pos_fila][pos_columna] = valor;
     }
+
+    Matriz operator+(Matriz other) const{
+        Matriz<T> temp(filas,columnas);
+        for (int i = 0; i < filas; ++i) {
+            for (int j = 0; j < columnas; ++j)
+                temp.set(i,j,matriz[i][j] + other.get(i,j));
+        }
+        return temp;
+    }
+
+    Matriz&operator=(Matriz other){
+        for (int i = 0; i < filas; ++i) {
+            for (int j = 0; j < columnas; ++j) {
+                matriz[i][j] = other.get(i,j);
+            }
+        }
+    }
+
+    bool igual_dimension(Matriz other){
+        return ((filas == other.get_filas()) && (columnas == other.get_columnas()));
+    }
+
     void print_matriz(){
         for (int i = 0; i < filas; ++i){
             for (int j = 0; j < columnas; ++j)
