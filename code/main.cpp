111a9a7cdc525359da8f7a6895706a3d0872ecdb
JeanleeRoy <49317699+JeanleeRoy@users.noreply.github.com>
2019-06-22 14:00:36 -0500

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -11,11 +11,11 @@ using namespace std::chrono;
 
 int main() {
     // NO CONCURRENTE
-    /*Matriz<int> m1(1000,1000), m2(1000,1000), m3(1000,1000);
+    Matriz<int> m1(1000,1000), m2(1000,1000), m3(1000,1000);
     m1.print_matriz(); m2.print_matriz();
     auto start = high_resolution_clock::now();
     m3 = m1*m2;
-    auto stop = high_resolution_clock::now();*/
+    auto stop = high_resolution_clock::now();
 
 
 //----------------------------------------------
